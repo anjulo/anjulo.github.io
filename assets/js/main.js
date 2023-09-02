3ab8f56af18709301e006bf81f01e8ab89f9284a
Bereket <faltamo.b@northeastern.edu>
2023-09-02 11:37:49 -0400

    Unlink unused css/js

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,21 +174,4 @@
     height: '90vh'
   });
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
 })()
